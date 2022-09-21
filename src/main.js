7790d0eb1a0ed9563eb57cda09c345cfa5fe7a33
huangyan321 <1604549268@qq.com>
2022-09-21 16:38:37 +0800

    fix:补充图片资源打包示例

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { sum, sub } from '@/js/math';
 import './css/lesstest.less';
 import './css/index.css';
+const img = document.createElement('img');
+img.src = require('./img/lanbitou.png').default;
+document.body.appendChild(img);
 console.log(sum(10, 20));
 console.log(sub(20, 10));
