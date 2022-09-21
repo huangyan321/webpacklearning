82f5fc525a3df88b488ca8ada775308dcca80a7c
huangyan321 <1604549268@qq.com>
2022-09-21 16:26:45 +0800

    feat:新增html-webpack-plugin,为bundle文件提供html文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -7,6 +8,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  plugins: [
+    //! 根据模板生成入口html
+    new HtmlWebpackPlugin({
+      title: 'lan bi tou',
+      template: './public/index.html',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
