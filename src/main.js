1c704a08facd885bca3560b33befe3420ea4807f
huangyan321 <1604549268@qq.com>
2022-09-21 16:33:24 +0800

    feat:新增样式相关loader&新增postcss-loader,提供browserslistrc为指定浏览器版本提供兼容性样式

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { sum, sub } from '@/js/math';
+import './css/lesstest.less';
+import './css/index.css';
 console.log(sum(10, 20));
 console.log(sub(20, 10));
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,44 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        //转换规则： 从下往上
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              //! 这个选项可以让css处理@import语法时回退到上一个loader再次对导入的文件做处理
+              importLoaders: 1,
+            },
+          },
+          //! 或者可以直接写一个配置文件 这样只需要postcss-loader就行
+          {
+            loader: 'postcss-loader',
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          'less-loader',
+        ],
+      },
+    ],
+  },
   plugins: [
     //! 根据模板生成入口html
     new HtmlWebpackPlugin({
