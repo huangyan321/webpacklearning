e3456f8dcec62e3f0e4691cd3deec0c0b7d57ab4
huangyan321 <1604549268@qq.com>
2022-09-22 11:15:01 +0800

    fix:解决esm和commonjs混用冲突

--- babel.config.js
+++ babel.config.js
@@ -8,9 +8,12 @@ module.exports = {
         //entry 手动在入口文件导入core-js/regenerator-runtime,根据目标浏览器引入对应polyfill
         useBuiltIns: 'usage',
         corejs: '3',
+        // targets: { ie: '10' },
       },
     ],
     ['@babel/preset-react'],
     ['@babel/preset-typescript'],
   ],
+  //esm和commonjs混用有冲突的话,使用'@babel/plugin-transform-modules-commonjs'插件
+  plugins: ['@babel/plugin-transform-modules-commonjs'],
 };
