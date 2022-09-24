4b1badb6e64c2ce44f56e8ae224ecec0cb6b1584
huangyan321 <1604549268@qq.com>
2022-09-24 20:32:24 +0800

    feat:开启webpack代理相关功能

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,23 @@ module.exports = {
     static: {
       directory: path.join(__dirname, 'public'),
     },
+    //为静态文件使用gzip压缩
     compress: true,
     port: 9000,
-  },
+    //为 带/api的请求开启代理
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        //将/api重写成空字符串
+        pathRewrite: {
+          '^/api': '',
+        },
+        //是否校验http正式是否合法
+        secure: false,
+        //改变http head的源为target
+        changeOrigin: true,
+      },
+    },
   module: {
     rules: [
       //解析js(x)
