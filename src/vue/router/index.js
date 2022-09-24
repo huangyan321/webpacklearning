f749e00ed8b0cf03feeb57d59deafc122b91042b
huangyan321 <1604549268@qq.com>
2022-09-25 03:08:45 +0800

    feat:懒加载分包配置预获取->浏览器空闲时下载

--- src/vue/router/index.js
+++ src/vue/router/index.js
@@ -1,7 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
-  { path: '/home', component: () => import(/* webpackChunkName: "home" */ './Home.vue') },
-  { path: '/about', component: () => import(/* webpackChunkName: "about" */ './About.vue') },
+  {
+    path: '/home',
+    component: () =>
+      import(
+        /* webpackChunkName: "home" */
+        /* webpackPrefetch: true */
+        './Home.vue'
+      ),
+  },
+  {
+    path: '/about',
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */
+        /* webpackPrefetch: true */
+        './About.vue'
+      ),
+  },
 ];
 const router = createRouter({
   routes,
