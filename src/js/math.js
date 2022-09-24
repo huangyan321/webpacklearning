2cb7d19c4a7edfe267502ccf9c8bb432980bfa9f
huangyan321 <1604549268@qq.com>
2022-09-25 02:45:55 +0800

    add:新增lodash测试示例

--- src/js/math.js
+++ src/js/math.js
@@ -1,9 +1,12 @@
+import _ from 'lodash';
+
 function sum(a, b) {
   return a + b;
 }
 function sub(a, b) {
   return a - b;
 }
+console.log(_.join([1, 2, 3, 4, 5, 6]));
 const res = new Promise((resolve, reject) => {
   resolve(1);
 });
