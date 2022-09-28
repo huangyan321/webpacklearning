454bfd1318e28122cfe0292d6f71cc0afddad7a2
huangyan321 <1604549268@qq.com>
2022-09-28 16:01:26 +0800

    add:自定义loader

---
+++ example/custom/customLoader/test.webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const resolve = function (url) {
+  // eslint-disable-next-line no-undef
+  return path.resolve(__dirname, url);
+};
+console.log(resolve('/dist/'));
+module.exports = {
+  entry: {
+    main: resolve('./src/index.js'),
+  },
+  output: {
+    filename: 'js/bundle.js',
+    //输出文件路径，必须是绝对路径
+    path: resolve('./dist'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: resolve('./loader/my-loader.js'),
+      },
+    ],
+  },
+};
