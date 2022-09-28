2cc42221b21130faceb121931a8c00b38020bfcd
huangyan321 <1604549268@qq.com>
2022-09-28 17:59:47 +0800

    update:增加注释

--- example/custom/customLoader/loader/my-babel-loader.js
+++ example/custom/customLoader/loader/my-babel-loader.js
@@ -1,16 +1,15 @@
 const babel = require('@babel/core');
 module.exports = function (content) {
   const options = this.getOptions();
-  let str = '';
   let callback = this.async();
+  console.log('================这里是babel-loader================');
   // eslint-disable-next-line @typescript-eslint/no-this-alias
   babel.transform(content, options, (err, res) => {
     if (err) {
       console.log(err);
       throw new Error(err);
     } else {
-      str = res.code;
-      callback(null, str);
+      callback(null, res.code);
     }
   });
 };
